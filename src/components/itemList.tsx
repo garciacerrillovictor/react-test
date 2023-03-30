8ee127536a176ca9b29f079d1387c3e6af8f2ef4
victor garcia <victor.garcia@kashio.net>
2023-03-30 01:59:51 -0500

    complete menu css.

--- src/components/itemList.tsx
+++ src/components/itemList.tsx
@@ -5,17 +5,17 @@ interface Props {
 const ItemList = ({children}: Props) => {
     const lista = children[0]
     const listkey = Object.keys(lista)
-
+console.log(lista)
     const renderItems = () :JSX.Element[] => {
         return listkey.map((key, index) => {
             return (
-                <li key={index}><a href="#">{ key }</a> {lista[key] > 0 ? <ItemList children={lista[key]} />:''}</li>
+                <li key={index}><a href="#">{ key }</a> {lista[key].length > 0 ? <ItemList children={lista[key]} />:''}</li>
             )
         })
     }
 
     return (
-        <ul className="subMenus">
+        <ul>
             {renderItems()}
         </ul>
     )
